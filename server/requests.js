34b9bb9cff981a29e1649b7b0809bb5d1d11b243
Thang Tong <tongthang0104@gmail.com>
2016-08-25 17:45:50 -0700

    [Fix] gitIgnore Api key

--- server/requests.js
+++ server/requests.js
@@ -1,16 +1,15 @@
 const Twitter = require('twitter');
-
+const key = require('apiKey.js')
 const client = new Twitter({
-  consumer_key: 'e9oSfxXtRTWObrcyB8dbKe8B4',
-  consumer_secret: 'o6gIdU576vUmYcqq3Bzk1Fo0LHJ0utKP6RcpTGOSxPXD7ORDAp',
-  access_token_key: '2909473447-WwTgSwukO5PNsJAMVQx1UgGlLvtFWJtvKlhq8JZ',
-  access_token_secret: 'Ps8CAqDuT7QxRV5pgj5pni2XpDTCzj6zFjoCHl9xQgdnJ'
+  consumer_key: key.TWITTER_CONSUMER_KEY,
+  consumer_secret: key.TWITTER_CONSUMER_SECRET,
+  access_token_key: key.TWITTER_ACCESS_TOKEN,
+  access_token_secret: key.TWITTER_ACCESS_TOKEN_SECRET
 });
- 
+
 const params = {screen_name: 'nodejs'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     console.log(tweets);
   }
 });
-
