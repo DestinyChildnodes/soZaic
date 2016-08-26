fa81cccf5713c0d56651ab9458ad0b5cd9cf8563
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-08-26 11:01:24 -0700

    Switch to angular-ui-router

--- client/app.js
+++ client/app.js
@@ -1,8 +1,30 @@
 `use strict`;
 
-angular.module(`sozaicApp`, [])
+angular.module(`sozaicApp`, [`ui.router`])
 
-.controller(`fbController`, function($scope) {
+.controller(`FbController`, function($scope) {
   console.log(`controller fb`)
   $scope.title = `facebook`;
-});
+  $scope.info = `1111111`;
+  console.log('hello world');
+  $scope.selected = function() {
+  }
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state(`fbFeed`, {
+      url: '/feed/facebook',
+      template: `<div>hello</div>`,
+      // controller: `FbController`
+
+      controller: function() {
+        console.log('hey')
+      }
+    })
+})
+
+.controller(`TwtrController`, function($scope) {
+  $scope.title = `twitter`;
+})
+;
