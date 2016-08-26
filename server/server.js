56d59c3977bf426fee0f58a0082bc42abacae0ed
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-08-26 13:26:39 -0700

    [Fix] conflict merge

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require(`path`);
 const app = express();
 
 app.use(express.static(`${__dirname}/../public/client`));
-// console.log(`${__dirname}/../node_modules`);
+
 // app.use(express.static(`${__dirname}\..\node_modules`));
 const port = process.env.PORT || 8080;
 app.listen(port);
