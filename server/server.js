60ebf143c9d04e5a8b5433438f032d6af3771e5d
Thang Tong <tongthang0104@gmail.com>
2016-08-25 17:30:05 -0700

    [Fix] server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,6 @@ const app = express();
 
 
 app.use(express.static(`${__dirname}/../client`));
-
+const port = process.env.PORT || 8080
 console.log(`Server is running on port 8080`);
 app.listen(8080);
