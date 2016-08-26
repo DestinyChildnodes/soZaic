cf5204763714630d2225f3b08db6ffc5411948a4
Thang Tong <tongthang0104@gmail.com>
2016-08-25 17:25:19 -0700

    [Fix] server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
-const Twitter = require('twitter');
+
 
 app.use(express.static(`${__dirname}/../client`));
 
 console.log(`Server is running on port 8080`);
 app.listen(8080);
-
-
