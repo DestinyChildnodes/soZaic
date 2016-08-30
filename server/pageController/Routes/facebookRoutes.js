658f4f9424cbd56b0cd7d800579374eccdb46c5f
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-08-30 16:24:16 -0700

    [Comment] Directions in facebookRoutes.js to config for FB local-node-auth

--- server/pageController/Routes/facebookRoutes.js
+++ server/pageController/Routes/facebookRoutes.js
@@ -27,3 +27,13 @@ module.exports = function(appRoute, passport, key) {
     }
   )
 };
+
+/*
+FB auth wouldn't allow to use localhost or 127.0.0.1.
+Didn't want to have to push to heroku every time for dev purposes.
+Had to change OS system admin file to get to work.
+Must use local.host for local-node-server, works for twitter also.
+These links helpful to make work:
+http://stackoverflow.com/questions/21310648/facebook-app-this-must-be-derived-from-canvas-url-secure-canvas-url
+https://support.rackspace.com/how-to/modify-your-hosts-file/
+*/
