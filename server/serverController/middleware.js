f3ec56888fe9630219c37e4069515a1302465d65
Thang Tong <tongthang0104@gmail.com>
2016-08-30 11:26:02 -0700

    [Fix] DRY code & create more Routers

--- server/serverController/middleware.js
+++ server/serverController/middleware.js
@@ -2,27 +2,26 @@ const bodyParser  = require('body-parser');
 const session = require('express-session');
 
 
-module.exports = function(app, express, passport, key) {
+module.exports = (app, express, passport, key) => {
   const twitterRouter = express.Router();
+  const youTubeRouter = express.Router();
+  const routeHelper = option => `../pageController/Routes/${option}`;
+
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}));
-
-  const oAuthYouTube = express.Router();
-  // const youTube = express.router();
-
-
-  //TODO: need facebook, instagram, youtube
-
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   app.use(express.static(`${__dirname}/../../client`));
 
-  //TODO: Need to add passport OAUTH
-
-
+  //Routing
+  app.use(`/api/youTube`, youTubeRouter);
   app.use(`/api/twitter`, twitterRouter);
+  // app.use(`/api/instagram`, instagramRouter);
+  // app.use(`/api/facebook`, facebookRouter);
 
-
-  require(`../pageController/Routes/twitterRoutes`)(twitterRouter, passport, key);
+  require(routeHelper(`twitterRoutes`))(twitterRouter, passport, key);
+  require(routeHelper(`youTubeRoutes`))(youTubeRouter, passport, key);
+  // require(routeHelper(`facebookRoutes`))(youTubeRouter, passport, key);
+  // require(routeHelper(`instagramRoutes`))(youTubeRouter, passport, key);
 }
