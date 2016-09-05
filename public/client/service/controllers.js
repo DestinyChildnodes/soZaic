4e04dc363d27fd3d2d5bc29d52cb754b90473b20
Thang Tong <tongthang0104@gmail.com>
2016-09-04 17:31:26 -0700

    [Fix] Get rid of debug Logs

--- public/client/service/controllers.js
+++ public/client/service/controllers.js
@@ -3,16 +3,11 @@
 angular.module(`sozaicApp.controller`, [`sozaicApp.serviceFactories`])
 
 .controller(`FbController`, function ($scope, GetFeed) {
-  console.log('hello FB Controller');
   $scope.posts = [];
   $scope.specificAction = (dataObj) => {
-    console.log('spAc controller activated');
-    // console.log(type);
-    console.log(GetFeed.fbSpAction);
-    console.log(GetFeed.fbFeed);
+
     GetFeed.fbSpAction(dataObj).then(function(resp) {
-      console.log(`FB spAct controllers `);
-      console.log(resp);
+
       if (resp) {
         console.log(resp);
       }
@@ -23,8 +18,7 @@ angular.module(`sozaicApp.controller`, [`sozaicApp.serviceFactories`])
   $scope.authFB = () => GetFeed.authFB();
   $scope.fbFeed = () => {
     GetFeed.fbFeed().then(function(resp) {
-      console.log(`FB Feed Controller`);
-      console.log(resp);
+
       if (resp) {
         $scope.posts = resp.data;
       }
@@ -37,12 +31,10 @@ angular.module(`sozaicApp.controller`, [`sozaicApp.serviceFactories`])
 .controller('YouTubeController', function($scope, GetFeed) {
   $scope.title = `youtube`;
   $scope.videos = [];
-  console.log('hello world');
+
   $scope.authYouTube = () => GetFeed.authYouTube();
   $scope.youTubeFeed = function() {
     GetFeed.youTubeFeed().then(function(response) {
-      console.log("testing response", response);
-
       let channels = response.data.items;
       for (let video of channels) {
         $scope.videos.push(video.id.videoId);
@@ -68,7 +60,6 @@ angular.module(`sozaicApp.controller`, [`sozaicApp.serviceFactories`])
   $scope.authInstagram = () => GetFeed.authInstagram();
   $scope.instagramFeed = () => {
     GetFeed.instagramFeed().then(function(response) {
-      console.log(response.data.data)
       $scope.photos = response.data.data;
     })
   }
@@ -85,13 +76,9 @@ angular.module(`sozaicApp.controller`, [`sozaicApp.serviceFactories`])
   $scope.tweets = [];
   $scope.authTwitter = () => GetFeed.authTwitter();
   $scope.twitterFeed = () => {
+
     GetFeed.twitterFeed().then(function(response) {
-      console.log(response);
       $scope.tweets = response.data;
-      console.log(`''''''''''''''''''''''''''''''''`);
-      console.log($scope.tweets[0]);
-      console.log(`''''''''''''''''''''''''''''''''`);
     }).catch(err => console.error(err));
   }
-
 })
