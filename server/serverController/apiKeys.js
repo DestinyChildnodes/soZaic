b0336d47ab6d097e069eae04061c88d173943e74
Martin <martinkkwan@gmail.com>
2016-09-06 14:49:07 -0700

    [Feature] Mixed view works for twitter, instagram, and youtube

--- server/serverController/apiKeys.js
+++ server/serverController/apiKeys.js
@@ -2,23 +2,23 @@
 
 module.exports = {
 
-  twitter: {
-    TWITTER_CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY,
-    TWITTER_CONSUMER_SECRET: process.env.TWITTER_CONSUMER_SECRET
-  },
+ twitter: {
+   TWITTER_CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY,
+   TWITTER_CONSUMER_SECRET: process.env.TWITTER_CONSUMER_SECRET
+ },
 
-  instagram: {
-    ClientID: process.env.instaClientId,
-    ClientSecret: process.env.instaClientSecret
-  },
+ instagram: {
+   ClientID: process.env.instaClientId,
+   ClientSecret: process.env.instaClientSecret
+ },
 
-  facebook: {
-    clientID : process.env.fbClientId,
-    clientSecret: process.env.fbClientSecret
-  },
+ facebook: {
+   clientID : process.env.fbClientId,
+   clientSecret: process.env.fbClientSecret
+ },
 
-  youtube: {
-    clientID: process.env.youtubeClientId,
-    clientSecret: process.env.youtubeClientSecret
-  }
+ youtube: {
+   clientID: process.env.youtubeClientId,
+   clientSecret: process.env.youtubeClientSecret
+ }
 }
